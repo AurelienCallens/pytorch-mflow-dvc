ec9064c824f618baf7279815d4f95a044bb62702
AurelienCallens <aurelien.callens@univ-pau.fr>
2022-12-20 15:19:15 +0100

    Add script to verify the number of channels in input images

---
+++ src/data/check_data.py
@@ -0,0 +1,18 @@
+import os
+import glob
+from torchvision.io import read_image
+
+@mlflow_run
+def verify_channel_number(data_repo):
+    list_images = glob.glob(data_repo, recursive=True)
+
+    for image in list_images:
+        img = read_image(image)
+        if img.shape[0] != 3:
+            os.remove(img)
+            print('Removed {image_path} with {img_channel} channels'.format(image_path=image, img_channel=img.shape[0]))
+
+    print("All the images have been verified!")
+
+if __name__ == '__main__':
+    verify_channel_number(data_repo='./data/raw/**/*.jpg')
